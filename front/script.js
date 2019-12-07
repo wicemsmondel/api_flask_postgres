41cf17862f1871f7eebe2c32fbf91061f52d417e
wicemsmondel <w.smondel@gmail.com>
2019-12-07 17:57:30 +0100

    no CDN version + logo

--- front/script.js
+++ front/script.js
@@ -5,29 +5,29 @@ $(document).ready(function () {
 });
 
 
-////////////////////  GET   ////////////////////  
+////////////////////  GET  ////////////////////  
 
 function showAll() {
   $.get('http://localhost:5000/data', function (data) {
     data = JSON.parse(data)
     $('#List').empty()
     console.log(data)
-    for (var i = 0; i < data.length; i++) {
+    data.forEach(data => {
       $('#List').append("<tr>")
-      $('#List').append("<th>" + "#" + data[i].Id + "</th>")
-      $('#List').append("<td>" + data[i].Name + "</td>")
-      $('#List').append("<td>" + data[i].Owner + "</td>")
-      $('#List').append("<td>" + data[i].User + "</td>")
-      $('#List').append("<td>" + data[i].Password + "</td>")
-      $('#List').append("<td>" + data[i].Namespace + "</td>")
-      $('#List').append("<td>" + "<button  id='deleterow' iddb=" + data[i].Id + " type='button' " + "onclick='deleteDB(this)'" + "class='btn btn-danger'>Delete</button></td></tr>")
-      $('#List').append("</tr>")
-    }
+      $('#List').append("<th>" + "#" + data.Id + "</th>")
+      $('#List').append("<td>" + data.Name + "</td>")
+      $('#List').append("<td>" + data.Owner + "</td>")
+      $('#List').append("<td>" + data.User + "</td>")
+      $('#List').append("<td>" + data.Password + "</td>")
+      $('#List').append("<td>" + data.Namespace + "</td>")
+      $('#List').append("<td>" + "<button  id='deleterow' iddb=" + data.Id + " type='button' " + "onclick='deleteDB(this)'" + "class='btn btn-danger'>Delete</button></td>")
+      // $('#List').append("</tr>")
+    });  
   });
 }
 
 
-////////////////////  POST   ////////////////////  
+////////////////////  POST  ////////////////////  
 
 function postPutain() {
   $('#post-data').click(function () {
@@ -46,7 +46,8 @@ function postPutain() {
 };
 
 
-//DELETE
+////////////////////  DELETE ////////////////////  
+
 function deleteDB(ctl) {
   console.log($(ctl).attr('iddb'))
   var Id = $(ctl).attr('iddb')
